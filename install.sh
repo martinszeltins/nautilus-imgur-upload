ef3e2838852785b3393ae38de21b08f54848992e
Martins <martins.zeltins@arkbauer.com>
2023-04-17 17:01:02 +0300

    Install script

--- install.sh
+++ install.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
+# Define variables
+SCRIPT_NAME="Upload to Imgur"
+SRC_DIR="$(pwd)"
+DST_DIR="$HOME/.local/share/nautilus/scripts"
+ICON_NAME="imgur.png"
+ICON_SRC="$SRC_DIR/$ICON_NAME"
+ICON_DST="$HOME/.local/share/icons/hicolor/128x128/apps"
 
+# Create the destination directories if they don't exist
+mkdir -p "$DST_DIR"
+mkdir -p "$ICON_DST"
+
+# Copy the "Upload to Imgur" script to the destination directory
+cp "$SRC_DIR/$SCRIPT_NAME" "$DST_DIR/$SCRIPT_NAME"
+
+# Make the script executable
+chmod +x "$DST_DIR/$SCRIPT_NAME"
+
+# Copy the icon to the destination directory
+cp "$ICON_SRC" "$ICON_DST/$ICON_NAME"
+
+# Set the icon for the "Upload to Imgur" script
+gio set -t string "$DST_DIR/$SCRIPT_NAME" "metadata::custom-icon" "file://$ICON_DST/$ICON_NAME.png"
+
+echo "Installation complete. Now you can right-click on an image in Nautilus and select Scripts -> Upload to Imgur."
